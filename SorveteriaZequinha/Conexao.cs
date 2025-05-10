acaf541dfc1cdf11311a282523577897d4c38dc4
Jackstiven <159174736+Jackstiven@users.noreply.github.com>
2025-05-09 22:08:26 -0300

    Add files via upload

---
+++ SorveteriaZequinha/Conexao.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace SorveteriaZequinha
+{
+    public class Conexao
+    {
+
+        private static string conectionString = "Server=localhost;Port=3306;Database=dbgelatosjegue;Uid=root;Pwd=''";
+
+        private static MySqlConnection conn = null;
+
+        public static MySqlConnection obterConexao()
+        {
+
+            conn = new MySqlConnection(conectionString);
+            try
+            {
+                conn.Open();
+
+            }
+            catch (SqlException)
+            {
+                conn = null;
+            }
+
+            return conn;
+
+        }
+    }
+}
